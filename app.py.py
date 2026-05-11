d7376507643d2bee7c52a33b8d7fb0e7b4f1840a
Aanuj04 <163642533+Aanuj04@users.noreply.github.com>
2026-05-11 13:35:22 +0530

    Update app.py

--- app.py.py
+++ app.py.py
@@ -4,7 +4,7 @@ import requests
 import pandas as pd
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=[your api key]".format(movie_id)
     try:
         data = requests.get(url, timeout=100)  # Increase timeout to 100 seconds
         data.raise_for_status()  # Raise exception for HTTP errors
